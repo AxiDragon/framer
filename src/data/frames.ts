574f3846b6b8ce2922215d1659c0cbf4e17aca4d
Axi <axidreamdragon@gmail.com>
2025-03-24 18:54:11 +0100

    Update frames

--- src/data/frames.ts
+++ src/data/frames.ts
@@ -1,23 +1,33 @@
 import Frame from "../components/Frame";
 
 import frame1 from "../assets/frames/frame1.png";
-import corner1 from "../assets/frames/corner1.webp";
-import edge1 from "../assets/frames/edge1.webp";
+import corner1 from "../assets/frames/corner1.png";
+import edge1 from "../assets/frames/edge1.png";
 
 import frame2 from "../assets/frames/frame2.png";
-import corner2 from "../assets/frames/corner2.webp";
-import edge2 from "../assets/frames/edge2.webp";
+import corner2 from "../assets/frames/corner2.png";
+import edge2 from "../assets/frames/edge2.png";
 
 import frame3 from "../assets/frames/frame3.png";
-import corner3 from "../assets/frames/corner3.webp";
-import edge3 from "../assets/frames/edge3.webp";
+import corner3 from "../assets/frames/corner3.png";
+import edge3 from "../assets/frames/edge3.png";
+
+import frame4 from "../assets/frames/frame4.png";
+import corner4 from "../assets/frames/corner4.png";
+import edge4 from "../assets/frames/edge4.png";
+
+import frame5 from "../assets/frames/frame5.png";
+import corner5 from "../assets/frames/corner5.png";
+import edge5 from "../assets/frames/edge5.png";
 
 import empty from "../assets/frames/empty.webp";
 
 const frames: Frame[] = [
-	new Frame(frame1, corner1, edge1),
+	new Frame(frame1, corner1, empty),
 	new Frame(frame2, corner2, edge2),
 	new Frame(frame3, corner3, edge3),
+	new Frame(frame4, corner4, edge4),
+	new Frame(frame5, corner5, edge5),
 ];
 
 export const EMPTY_FRAME = new Frame(empty, empty, empty);
